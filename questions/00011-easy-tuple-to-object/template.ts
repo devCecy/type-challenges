2e2bf9838b553738b9b0017354a04ef07be5e1d9
devCecy <im.ceciliaan@gmail.com>
2022-12-10 14:15:43 +0900

    [Answer] : 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,9 @@
-type TupleToObject<T extends readonly any[]> = any
+// type TupleToObject<T extends readonly any[]> = any
+
+/**
+ * @kennysliding 풀이
+ * we will first check the types in T to see if its either string/number/symbol, by using the built-in type PropertyKey, then we can use that to the interface and loop over the types in T by using T[number], and assign that to P
+ */
+type TupleToObject<T extends readonly PropertyKey[]> = {
+	[P in T[number]]: P
+}
