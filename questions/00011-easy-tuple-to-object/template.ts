53da727297c101664b83424a636d6c6d2e8b6c82
devCecy <im.ceciliaan@gmail.com>
2022-12-12 21:51:26 +0900

    [Answer] : 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -3,6 +3,7 @@
 /**
  * @kennysliding 풀이
  * we will first check the types in T to see if its either string/number/symbol, by using the built-in type PropertyKey, then we can use that to the interface and loop over the types in T by using T[number], and assign that to P
+ * (built-in type) type PropertyKey = string | number | symbol
  */
 type TupleToObject<T extends readonly PropertyKey[]> = {
 	[P in T[number]]: P
